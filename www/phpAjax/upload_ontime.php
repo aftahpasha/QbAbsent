a76bffbd84f93fee80730bcb4784ea03d0b68b40
Aftah Mac <AftahMac@Aftahs-MacBook-Pro.local>
2018-08-01 11:21:14 +0700

    File PHP untuk Backend

--- www/phpAjax/upload_ontime.php
+++ www/phpAjax/upload_ontime.php
@@ -1,37 +1,46 @@
 <?php
 
-// myqli_connect('127.0.0.1:3306', 'root','','AbAbsent');
+header('Access-Control-Allow-Origin: *');
+$servername = "localhost";
+$username = "aftahpasha";
+$password = "";
+$dbname = "QbAbsen";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$jam_datang = $_GET['jam_datang'] - 7;
+$employee_id = $_GET['employee_id'];
+$punch_in_utc_time = $_GET['tahun']."-".$_GET['bulan']."-".$_GET['tanggal']." ".$jam_datang.":".$_GET['menit_datang'].":00";
+$punch_in_note = $_GET['alasanTelat'];
+$punch_in_time_offset = 7;
+$punch_in_user_time = $_GET['tahun']."-".$_GET['bulan']."-".$_GET['tanggal']." ".$_GET['jam_datang'].":".$_GET['menit_datang'].":00";
+$jam_pulang = $_GET['jam_pulang'] - 7;
+$punch_out_utc_time = $_GET['tahun']."-".$_GET['bulan']."-".$_GET['tanggal']." ".$jam_pulang.":".$_GET['menit_pulang'].":00";
+$punch_out_note = "TEPAT WAKTU";
+$punch_in_time_offset = 7;
+$punch_out_user_time = $_GET['tahun']."-".$_GET['bulan']."-".$_GET['tanggal']." ".$_GET['jam_pulang'].":".$_GET['menit_pulang'].":00";
+$state = "PUNCHED OUT";
 
 
 
-$mysqli = mysqli_connect("localhost", "aftahpasha", "", "QbAbsen");
+$punch_in = date_create($punch_in_utc_time,date_interval_create_from_date_string("-7 HOUR"));
+$punch_out = date_add($punch_out_utc_time,date_interval_create_from_date_string("-7 HOUR"));
 
-$query = "INSERT INTO test VALUES ('$_GET[email]','$_GET[name]','$_GET[tanggal]','$_GET[bulan]','$_GET[tahun]','$_GET[jam_datang]','$_GET[menit_datang]','$_GET[jam_pulang]','$_GET[menit_pulang]','TEPAT WAKTU')";
-$exec = mysqli_query($mysqli,$query);
+$sql = "INSERT INTO ohrm_attendance_record (`id`, `employee_id`, `punch_in_utc_time`, `punch_in_note`,`punch_in_time_offset`,`punch_in_user_time`,
+`punch_out_utc_time`,`punch_out_note`,`punch_out_time_offset`, `punch_out_user_time`, `state`)
+VALUES (NULL, '".$employee_id."','".$punch_in_utc_time."','".$punch_in_note."','".$punch_in_time_offset."','".$punch_in_user_time."','".$punch_out_utc_time."','".$punch_out_note."','".$punch_out_time_offset."','".$punch_out_user_time."','".$state."')";
 
+echo $sql;
 
-
-if (!$exec) {
-  $myObj->error = "GAGAL";
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-$myObj->jam_datang = $_GET['jam_datang'];
-$myObj->email = $_GET['email'];
-$myObj->name = $_GET['name'];
-$myObj->tanggal = $_GET['tanggal'];
-$myObj->bulan = $_GET['bulan'];
-$myObj->menit_datang = $_GET['menit_datang'];
-$myObj->jam_pulang = $_GET['jam_pulang'];
-$myObj->menit_pulang = $_GET['menit_pulang'];
-$myObj->keterangan = $_GET['keterangan'];
-$myObj->age = 30;
-$myObj->city = "New York";
-
-$myJSON = json_encode($myObj);
-
-echo $myJSON;
-
-
-
-
- ?>
+$conn->close();
+?>
